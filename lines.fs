342480247b81151575ab8eb6a01956b346d01271
alkesnaf <alkesnaf@buffalo.edu>
2025-04-25 00:59:59 -0400

    added vertical line words for characters

--- lines.fs
+++ lines.fs
@@ -9,3 +9,18 @@
   rel-y
   lift-pen
 ;
+
+( stem - used for: B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, T, U )
+: stem ( -- )
+  15 rel-y
+;
+
+( used for: A, Y )
+: leg ( -- )
+  12 rel-y
+;
+
+( used for: S, R, P, G )
+: half-stem ( -- )
+  8 rel-y
+;
