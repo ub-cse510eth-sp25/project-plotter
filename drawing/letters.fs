cff6dc8eedaec67f43aab958d3901c51221e0468
John Abramo <jmabramo@buffalo.edu>
2025-04-30 15:51:14 -0400

    add letters

--- drawing/letters.fs
+++ drawing/letters.fs
@@ -11,6 +11,80 @@
   ( move to top right )
 ;
 
+: plot-E ( -- )
+    \ assumes starting at botom left corner
+    10 rel-x
+    pen-down
+    -10 rel-x
+    15 rel-y
+    10 rel-x
+    pen-up
+    -10 -7 move-to-rel
+    pen-down
+    8 rel-x
+    pen-up
+    7 2 move-to-rel
+    \ finishes at the top right corner
+;
+
+: plot-F ( -- )
+    \ assumes starting at botom left corner
+    pen-down
+    15 rel-y
+    10 rel-x
+    pen-up
+    -10 -7 move-to-rel
+    pen-down
+    8 rel-x
+    pen-up
+    7 2 move-to-rel
+    \ finishes at the top right corner
+;
+
+: plot-H ( -- )
+    \ assumes starting at botom left corner
+    pen-down
+    15 rel-y
+    pen-up
+    -7 rel-y
+    pen-down
+    10 rel-x
+    pen-up
+    -8 rel-y
+    pen-down
+    15 rel-y
+    pen-up
+    \ finishes at the top right corner
+;
+
+: plot-I ( -- )
+    \ assumes starting at botom left corner
+    pen-down
+    10 rel-x
+    pen-up
+    -5 rel-x
+    pen-down
+    15 rel-y
+    pen-up
+    -5 rel-x
+    pen-down
+    10 rel-x
+    pen-up
+    \ finishes at the top right corner
+;
+
+: plot-L ( -- )
+    \ assumes starting at botom left corner
+    10 rel-x
+    pen-down
+    -10 rel-x
+    15 rel-y
+    pen-up
+    10 rel-x
+    \ finishes at the top right corner
+;
+
+
 : plot-M ( -- )
   15 rel-y
   pen-down
@@ -30,6 +104,19 @@
   reverse-stem
   pen-up
   ( move to top right )
+;
+
+: plot-T ( -- )
+    \ assumes starting at botom left corner
+    5 rel-x
+    pen-down
+    15 rel-y
+    pen-up
+    -5 rel-x
+    pen-down
+    10 rel-x
+    pen-up
+    \ finishes at the top right corner
 ;
 
 : plot-V ( -- )
