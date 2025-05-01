e79d51ccaf0cb22d564ecba4ac10ce01ad27a79c
sabrina <82106636+sabjohnson@users.noreply.github.com>
2025-04-30 20:13:45 -0400

    attempt 4 :)

--- utils/terminal.fs
+++ utils/terminal.fs
@@ -3,12 +3,25 @@
   27 emit [char] [ emit
 ;
 
+: .digit ( n -- )
+  [char] 0 + emit
+;
+
+: .without-space ( n -- )
+  dup 10 / dup if
+    .digit
+  else
+    drop
+  then
+  10 mod .digit
+;
+
 : go-to-position ( row col -- )
   esc[
-  swap u. drop       \ Print row without space
-  [char] ; emit      \ Print semicolon
-  u. drop            \ Print col without space
-  [char] H emit      \ Print H
+  swap .without-space
+  [char] ; emit
+  .without-space
+  [char] H emit
 ;
 
 : clear-screen-from-cursor ( -- )
